86a4213ab07311403c8ba9a698860b71bba5209e
Llnk3r <61619729+Llnk3r@users.noreply.github.com>
2021-03-22 20:24:48 +0200

    Add files via upload

---
+++ Азот/js/script.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+var modal = document.getElementById("myModal2");
+var btn = document.getElementById("myBtn2");
+var span = document.getElementsByClassName("close2")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+span.onclick = function() {
+  modal.style.display = "none";
+};
