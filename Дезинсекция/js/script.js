c0ec7f33e22816e8fa2d71474c7352985e73c384
Llnk3r <61619729+Llnk3r@users.noreply.github.com>
2021-03-22 20:06:12 +0200

    Add files via upload

---
+++ Дезинсекция/js/script.js
@@ -0,0 +1,69 @@
+$(".toggle").click(function (e) {
+  e.preventDefault();
+  $(".mobile__menu").toggleClass("active");
+});
+
+$(".mobile__toggle").click(function (e) {
+  e.preventDefault();
+  $(".mobile__menu").toggleClass("active");
+});
+
+$(".toggle").click(function (e) {
+  e.preventDefault();
+  $(".box-shadow").toggleClass("shadow");
+});
+
+$(".mobile__toggle").click(function (e) {
+  e.preventDefault();
+  $(".box-shadow").toggleClass("shadow");
+});
+
+$(".toggle").click(function (e) {
+  e.preventDefault();
+  $(".main").toggleClass("z-index");
+});
+
+$(".mobile__toggle").click(function (e) {
+  e.preventDefault();
+  $(".main").toggleClass("z-index");
+});
+
+$(".toggle").click(function (e) {
+  e.preventDefault();
+  $(".footer").toggleClass("z-index");
+});
+
+$(".mobile__toggle").click(function (e) {
+  e.preventDefault();
+  $(".footer").toggleClass("z-index");
+});
+
+$(".main__icon-open-1").click(function (e) {
+  e.preventDefault();
+  $(".main__p-1").toggleClass("main__open-show");
+});
+
+$(".main__icon-open-1").click(function (e) {
+  e.preventDefault();
+  $(".main__icon-open-1").toggleClass("main__icon-close-1");
+});
+
+$(".main__icon-open-2").click(function (e) {
+  e.preventDefault();
+  $(".main__p-2").toggleClass("main__open-show");
+});
+
+$(".main__icon-open-2").click(function (e) {
+  e.preventDefault();
+  $(".main__icon-open-2").toggleClass("main__icon-close-2");
+});
+
+$(".main__icon-open-3").click(function (e) {
+  e.preventDefault();
+  $(".main__p-3").toggleClass("main__open-show");
+});
+
+$(".main__icon-open-3").click(function (e) {
+  e.preventDefault();
+  $(".main__icon-open-3").toggleClass("main__icon-close-2");
+});
